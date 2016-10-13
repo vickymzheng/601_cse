cdc986548d9bb9a069c1ba1d4ed78aa56a94cfc9
vickyzheng94 <vickyzheng94@gmail.com>
2016-10-13 12:27:26 -0400

    move formatting

--- hw2/sample_contain.m
+++ hw2/sample_contain.m
@@ -3,26 +3,26 @@ function [ contain ] = sample_contain(union_set,this_sample)
 %   union_set is 1*k
 %   this_sample is 1*item_nums, here is 1*101
 
-k = size(union_set,2);
-contain = 1;
+    k = size(union_set,2);
+    contain = 1;
 
-for i = 1:k
-    if union_set(i) <= 200
-        index = floor ( ( union_set(i) + 1 ) / 2 );
-        if this_sample(index) ~= mod ( union_set(i),2)
-            contain = 0;
-            break;
+    for i = 1:k
+        if union_set(i) <= 200
+            index = floor ( ( union_set(i) + 1 ) / 2 );
+            if this_sample(index) ~= mod ( union_set(i),2)
+                contain = 0;
+                break;
+            end
+            
         end
         
-    end
-    
-    if union_set(i) > 200
-        if this_sample(101) ~= ( union_set(i)-200)
-            contain = 0;
-            break;
+        if union_set(i) > 200
+            if this_sample(101) ~= ( union_set(i)-200)
+                contain = 0;
+                break;
+            end
         end
     end
-end
 
 end
 
