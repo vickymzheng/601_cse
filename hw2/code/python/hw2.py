f6203423d7078299e087142c8f89c1e6b10c205d
vickyzheng94 <vickyzheng94@gmail.com>
2016-10-14 13:04:09 -0400

    some documentation

--- hw2/code/python/hw2.py
+++ hw2/code/python/hw2.py
@@ -31,7 +31,7 @@ def hw2(inputFileName):
 		fileContents.append(unLabeledData)
 		line = toRead.readline()
 
-
+	#This is to clean the data for int comparisons rather than string comparisons 
 	for i in range(0, len(fileContents)):
 		for j in range(0, len(fileContents[i])):
 			if (fileContents[i][j] == "UP"):
@@ -51,6 +51,7 @@ def hw2(inputFileName):
 	frequencyCounter = [0]*204 
 	for x in fileContents:
 		for y in x:
+			#This is to count the frequency of each int found. G1_UP will be at 0 so every time we find G1_UP, we increment frequencyCounter[0]
 			frequencyCounter[y]+=1
 
 	support = 30
