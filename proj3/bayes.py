26f689bea827a2188c230534a56919caff8a3a19
vickyzheng94 <vickyzheng94@gmail.com>
2016-11-21 14:37:58 -0500

    turning data into floats when necessary. Deciding how to partition data now

--- proj3/bayes.py
+++ proj3/bayes.py
@@ -1,12 +1,39 @@
+def partitionData(data):
+	numSamples = len(data)
+	numAttributes = len(data[0])
+
+
+	for attributeNum in range(0, numAttributes):
+		if (isNumeric(data[0][attributeNum])):
+			for sampleNum in range(0, numSamples):
+				data[sampleNum][attributeNum] = float(data[sampleNum][attributeNum])		
+
+	return data
+
 def getData(fileName):
 	toRead = open(fileName)
 	centroids = []
 	line = toRead.readline()
 	while line != "":
-        centroid = [line.split()]
-        centroids.append(centroid)
-        line = toRead.readline()
-    return centroids
+	    centroid = line.split()
+	    centroids.append(centroid)
+	    line = toRead.readline()
 
-data = getData("project3_dataset1.txt")
-data = getData("project3_dataset2.txt")
+	centroids = partitionData(centroids)
+	
+	return centroids
+
+def isNumeric(potentialNumber):
+	try:
+		float(potentialNumber)
+		return True
+	except ValueError:
+		return False
+
+def bayes(fileName):
+	data = getData(fileName)
+
+
+# bayes("project3_dataset1.txt")
+#bayes("project3_dataset2.txt")
+getData("project3_dataset2.txt")
