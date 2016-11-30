1c41469dedf6cccc11034c11ebccfc3135f4a5e0
vickyzheng94 <vickyzheng94@gmail.com>
2016-11-29 22:57:58 -0500

    submitted everything for hw3

---
+++ Homework3/Homework3/Code/ReadMe.txt
@@ -0,0 +1,17 @@
+1. There are 3 different python program for this project, the names are hw3_dataSet1.py, hw3_dataset2.py, hw3_dataset3.py. The first program is for attweb_net, the second is for physics_collaboration_net and the third is for yeast_undurected_metabolic. All the program are written with python 2.7.
+
+2. To run the code, just make sure the txt file of the dataset is in the same folder with the code.
+
+3. You can change k and r in the code to get different clustering result.
+
+4. The program use sklearn package to normalize the data.
+
+5. The output of the code will be the cluster of each node, the sequence of the node is the same as the sequence of the nodes in the net file, which I believe is the sequence of the appearance of the node in the provided dataset file. It will also give the total number of the clusters.
+
+6. if you uncomment the bottom part of the code, it will output each node name and the cluster it belongs to.
+
+7. To get the flu file, I just simply copy the cluster sequence into the txt file and convert it to the cluster file and use Pajek to visualize it.
+
+
+
+Thank you very much for grading.
---
+++ Homework3/Homework3/Code/hw3_dataSet1.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 07:34:12 2016
+
+@author: yuzeliu
+"""
+import numpy as np
+#import random
+import math
+#import sys
+from sklearn import preprocessing
+
+def expand(data,k):
+    return np.linalg.matrix_power(data,k)
+
+def inflate(data,r):
+    for i in range(0,len(data)):
+        for j in range(0,len(data[0])):
+            data[i][j] = math.pow(data[i][j],r)
+    return preprocessing.normalize(data,norm = 'l1',axis = 0)
+
+def MCL(data,k,r):
+    data = expand(data,k)
+    data = inflate(data,r)
+    return data
+        
+
+#for attweb_net.txt, 180 nodes; physics_collaboration_net.txt,142 nodes; yeast, 359 nodes
+Inputfilename = 'attweb_net.txt'
+dataSet_pre = np.loadtxt(Inputfilename, delimiter = ' ')
+dataSet = np.zeros([len(dataSet_pre),2]) 
+assmat = np.zeros([180,180])
+narray = []
+
+for i in range(0,len(dataSet_pre)):
+    for j in range(0,2):
+        if dataSet_pre[i][j] in narray:
+            dataSet[i][j] = narray.index(dataSet_pre[i][j])
+        else:
+            narray.append(dataSet_pre[i][j])
+            dataSet[i][j] = len(narray) - 1
+
+for i in range(0,len(dataSet)): #construct associate matrix
+    m = int(dataSet[i][0])
+    n = int(dataSet[i][1])
+    assmat[m][n] = assmat[n][m] = 1
+
+for i in range(0,len(assmat)):  #add selfloop
+    assmat[i][i] = 1.0
+
+assmat_norm = preprocessing.normalize(assmat,norm = 'l1',axis = 0)
+
+repeat = 100
+k = 3
+r = 2
+cluster = 1
+nodeArray = np.zeros([len(assmat),2])
+#flag = 0
+
+for i in range(0,len(assmat)):
+    nodeArray[i][0] = i
+
+while repeat > 0:
+    assmat = MCL(assmat,k,r)
+    repeat = repeat - 1
+
+for i in range(0,len(assmat)):
+    for j in range(0,len(assmat)):
+        if assmat[i][j] != 0:
+            nodeArray[j][1] = cluster
+    cluster = cluster + 1
+
+
+label =[]
+temp = []
+
+for i in range(0,len(nodeArray)):
+    label.append(nodeArray[i][1])
+    
+for i in range(0,len(label)):
+    if label[i] in temp:
+        nodeArray[i][1] = temp.index(label[i]) + 1
+    else:
+        temp.append(label[i])
+        nodeArray[i][1] = len(temp)
+
+#for i in range(0,len(nodeArray)):
+#    print '{:5}'.format(nodeArray[i][0]), '{:18}'.format(narray[i]), '{:>4}'.format(nodeArray[i][1])
+print '*Vertices 180'
+for i in range(0,len(nodeArray)):
+    print ' ',int(nodeArray[i][1])
+
+print 'The total number of clusters are : ', len(temp)
+
+    
+         
+
